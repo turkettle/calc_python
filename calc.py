a54dc3ac38d8643134b14b415989d9e5dbdda101
root <root@localhost.localdomain>
2016-09-05 15:47:30 +0200

    Init commit.

---
+++ calc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+###################
+#@Python - Calculatrice
+#@Gaetan
+#@date 05092016
+###################
+
+print 'Bienvenu dans la calculatrice interactive python'
+
+int1 = int(input("Premier nombre: "))
+op = input("Opérateur: ")
+int1 = int(input("Deuxième nombre: "))
+
+ops = {'+': '+', 'x': '*', '-': '-', '/': '/'}
+
+if op in pos
+    print 'Fine'
