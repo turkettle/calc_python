93611c4d78dddc5a3369dce6b5ed817e9a0f7805
root <root@localhost.localdomain>
2016-09-06 10:38:34 +0200

    Amélioration des saisies.

--- calc.py
+++ calc.py
@@ -9,42 +9,50 @@
 ##      FONCTIONS       ##
 
 def calculate(a, b, op):
-
     """Exécution du calcul"""
 
-    if op == '1':
+    if op == 1:
         return a + b
 
-    elif op == '2':
+    elif op == 2:
         return a - b
 
-    elif op == '3':
+    elif op == 3:
         return a * b
 
-    elif op == '4':
+    elif op == 4:
         return a / b
 
+def launch_prompt():
+    """Affiche la ère étape du programme"""
 
-##      Affichage console       ##
+    print('\r\n[1]: {}'.format(ops[0]))
+    print('[2]: {}'.format(ops[1]))
+    print('[3]: {}'.format(ops[2]))
+    print('[4]: {}'.format(ops[3]))
+    print('\r\n[q]: Sortir')
+
+    return str(raw_input('\r\nChoisissez une opération: '))
+
+def exec_application(op):
+    """Traite les données envoyées par l'utilisateur"""
+
+    while op.lower() != 'q':
+
+        try:
+            op = int(op)
+        except ValueError:
+            print('\r\n--- Saisi opération incorrect. Veuillez réessayer ---')
+            op = str(launch_prompt())
+            continue
 
 
-ops = ('Addition', 'Soustraction', 'Multiplication', 'Division')
+        if int(op) < 1 or int(op) > 4:
+            print('\r\n--- Saisi opération incorrect. Veuillez réessayer ---')
+            op = str(launch_prompt())
+            continue
 
-print('\r\n[1]: {}'.format(ops[0]))
-print('[2]: {}'.format(ops[1]))
-print('[3]: {}'.format(ops[2]))
-print('[4]: {}'.format(ops[3]))
-print('\r\n[q]: Sortir')
-
-op = str(input('\r\nChoisissez une opération: '))
-
-while op.lower() != 'q':
-
-    if int(op) < 1 or int(op) > 4:
-        print('\r\nSaisi opération incorrect. Veuillez réessayer.')
-        continue
-
-    else:
+        # else:
         """Affichage du choix de calcul"""
         print('\r\n--- Vous avez choisi une {} ---'.format(ops[int(op) - 1]))
 
@@ -52,6 +60,20 @@ while op.lower() != 'q':
         a = int(input('\r\nChoix du 1er chiffre: '))
         b = int(input('\r\nChoix du 2ème chiffre: '))
 
-    print('\r\n--- Le résultat est {} ---'.format(str(calculate(a, b, op))))
+        print('\r\n--- Le résultat est {} ---'.format(str(calculate(a, b, op))))
+        op = str(launch_prompt())
 
-print("Bye bye !!")
+    print("Bye bye !!")
+
+
+##      Affichage console       ##
+
+try:
+    ops = ('Addition', 'Soustraction', 'Multiplication', 'Division')
+    op = str(launch_prompt())
+    exec_application(op)
+
+except KeyboardInterrupt:
+    """Capture l'erreur si l'utilisateur ferme le programme avec Ctrl+C"""
+    print '\r\n'
+    pass
